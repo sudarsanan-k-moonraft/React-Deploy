ae8208865c14f65ca5aa2a2eca1c647008179b21
sudarsanan-k-moonraft <sudarsanan.k@moonraft.com>
2022-10-18 19:12:35 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FirstPage from './components/dashboard/firstPage/firstPage';
+import { Route, Routes } from 'react-router-dom';
+import SearchDrugPage from './components/dashboard/searchDrugPage/searchDrugPage';
+import SearchPharmacyPage from './components/dashboard/searchPharmacy/searchPharmacy';
+import SearchProviderPage from './components/dashboard/searchProvider/searchProviderPage';
+import DisplayPharmacy from './components/common/displayPharmacy/displayPharmacy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Routes>
+        <Route path='' element={<FirstPage />}/>
+        <Route path="/SearchDrug" element={<SearchDrugPage />} />
+        <Route path="/SearchPharmacy" element={<SearchPharmacyPage />} />
+        <Route path="/SearchProvider" element={<SearchProviderPage />} />
+        <Route path="/PharmacyContent" element={<DisplayPharmacy />} />
+      </Routes>
+
+    </>
   );
 }
 
